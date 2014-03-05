5e9b5cdd3aa4d00d0ef1d96689ebf22af96f0db5
Wouter Groeneveld <wouter.groeneveld@gmail.com>
2014-03-05 11:55:55 +0100

    make things more "pythonic"! (listcomp, flat)

--- 01_calculatestring/python/calculator.py
+++ 01_calculatestring/python/calculator.py
@@ -1,25 +1,24 @@
 # python 3: Removed reduce(). Use functools.reduce() if you really need it; however, 99 percent of the time an explicit for loop is more readable.
+# http://www.artima.com/forums/flat.jsp?forum=106&thread=98196 no "pythonic way" of doing it.
 from functools import reduce
 
-class Calculator:
+ops = {
+	"+": {
+		"op": lambda x,y: x + y,
+		"init": 0
+	},
+	"*": {
+		"op": lambda x,y: x * y,
+		"init": 1
+	}
+};
 
-	ops = {
-		"+": {
-			"op": lambda x,y: x + y,
-			"init": 0
-		},
-		"*": {
-			"op": lambda x,y: x * y,
-			"init": 1
-		}
-	};
-
-	# is there a way to use JSON-like op.op instead of op["op"]?
-	def calculate(self, s):
-		if s == '':
-			return 0
-		for operator in self.ops.keys():
-			if s.find(operator) >= 0:
-				op = self.ops[operator]
-				return reduce(op["op"], map(int, s.split(operator)), op["init"])
-		return int(s)
+# is there a way to use JSON-like op.op instead of op["op"]?
+def calculate(s):
+	if s == '':
+		return 0
+	for operator in ops.keys():
+		if s.find(operator) >= 0:
+			op = ops[operator]
+			return reduce(op["op"], [int(v) for v in s.split(operator)], op["init"])
+	return int(s)
--- 01_calculatestring/python/test_calculator.py
+++ 01_calculatestring/python/test_calculator.py
@@ -1,25 +1,22 @@
 import unittest
-from calculator import Calculator
+from calculator import calculate
 
 class TestCalculator(unittest.TestCase):
 
-	def setUp(self):
-		self.calc = Calculator().calculate;
-
 	def test_calculateBasicNumberReturnsNumber(self):
- 		self.assertEqual(3, self.calc('3'))
+ 		self.assertEqual(3, calculate('3'))
 
 	def test_calculateEmptyStringShouldReturnZero(self):
-		self.assertEqual(0, self.calc(''))
+		self.assertEqual(0, calculate(''))
 
 	def test_calculateSimpleSumReturnsResult(self):
-		self.assertEqual(6, self.calc('4+2'))
+		self.assertEqual(6, calculate('4+2'))
 
 	def test_calculateSimpleMultiplicationReturnsResult(self):
-		self.assertEqual(10, self.calc('5*2'))
+		self.assertEqual(10, calculate('5*2'))
 
 	def test_calculateInvalidStringShouldThrowException(self):
-		self.assertRaises(ValueError, self.calc, ('blabl'))
+		self.assertRaises(ValueError, calculate, ('blabl'))
 
 if __name__ == '__main__':
 	unittest.main()
