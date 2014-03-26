84f0d9e83489bed6ef6078aa3a7a5ae2b09d0573
Wouter Groeneveld <wouter.groeneveld@gmail.com>
2014-03-26 11:40:36 +0100

    use fn* typedef

--- 01_calculatestring/C++/src/calculatestring.cc
+++ 01_calculatestring/C++/src/calculatestring.cc
@@ -27,43 +27,26 @@ vector<string> split(const string searchString, const char splitChar) {
     return strings;
 }
 
-// classes can be defined within the calculate() method
-// since C++11, no warning generated; see http://stackoverflow.com/questions/742607/using-local-classes-with-stl-algorithms
-class Operator {
-public:
-	virtual int op(int a, int b) = 0;
-};
+int sum(int a, int b) { return a + b; };
+int multiplication(int a, int b) { return a * b; };
+int division(int a, int b) { return a / b; };
 
-class Sum : public Operator {
-public:
-	inline int op(int a, int b) { return a + b; };
-};
-
-class Multiplication : public Operator {
-public:
-	inline int op(int a, int b) { return a * b; };
-};
-
-class Division : public Operator {
-public:
-	inline int op(int a, int b) { return a / b; };
-};
+typedef int (*opFn)(int, int);
 
 int calculate(const string s) {
 	if(s.empty()) {
 		return 0;
 	}
 
-	// is there a way to do this without pointers?
-	map<char, Operator*> ops;
-	ops['+'] = new Sum();
-	ops['*'] = new Multiplication();
-	ops['/'] = new Division();
+	map<char, opFn> ops;
+	ops['+'] = &sum;
+	ops['*'] = &multiplication;
+	ops['/'] = &division;
 
-	for(map<char,Operator*>::iterator it = ops.begin(); it != ops.end(); ++it) {
+	for(map<char, opFn>::iterator it = ops.begin(); it != ops.end(); ++it) {
 		if(s.find(it->first) != string::npos) {
 			vector<string> numbers = split(s, it->first);
-			return it->second->op(parseInt(numbers[0]), parseInt(numbers[1]));
+			return it->second(parseInt(numbers[0]), parseInt(numbers[1]));
 		}
 	}
 
